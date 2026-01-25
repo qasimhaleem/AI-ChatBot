224e5e00c40555da74f926baad31fc179035aada
Qasim Haleem <mrqasimhaleem@gmail.com>
2026-01-26 00:10:55 +0500

    PWA has been done to the site

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss(),
+  VitePWA({
+    registerType: "autoUpdate",
+    devOptions: {
+      enabled: true
+    },
+    manifest: {
+      name: "My React Vite App",
+      short_name: "ViteApp",
+      start_url: "/",
+      display: "standalone",
+      background_color: "#ffffff",
+      theme_color: "#0d6efd",
+      icons: [
+        {
+          src: "pwa-192x192.png",
+          sizes: "192x192",
+          type: "image/png"
+        },
+        {
+          src: "pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png"
+        }
+      ]
+    }
+  })
+  ],
 })
